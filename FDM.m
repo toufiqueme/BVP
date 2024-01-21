1ad8137549257c0491165e4125e1cf2512c031e8
Md. Toufikur Rahaman <74497538+toufiqueme@users.noreply.github.com>
2024-01-21 11:42:22 +0600

    Update FDM.m

--- FDM.m
+++ FDM.m
@@ -1,8 +1,7 @@
-
-N = 100; 
+N = 100;
 L = 10;
 h = L/N;
-x = linspace(0, L, N+1); 
+x = linspace(0, L, N+1);
 y = zeros(N+1, 1);
 
 a = ones(N+1, 1);
@@ -10,15 +9,19 @@ b = -2*ones(N+1, 1);
 c = ones(N+1, 1);
 d = -h^2*(x.^2+1);
 
-
 y(1) = 0;
 y(N+1) = 0;
 
-
 for i = 2:N
-    y(i) = (d(i) - a(i)*y(i-1) - c(i)*y(i+1)) / b(i)
+    y(i) = (d(i) - a(i)*y(i-1) - c(i)*y(i+1)) / b(i);
 end
 
+% Displaying the table
+table_data = [x', y];
+disp('Table of x and y values:');
+disp(table_data);
+
+% Plotting
 plot(x, y);
 xlabel('x');
 ylabel('y');
